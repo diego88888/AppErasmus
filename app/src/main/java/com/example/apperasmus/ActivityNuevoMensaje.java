8af51171b32835e1ee20d9d272e9fd2e85932149
diego88888 <45438301+diego88888@users.noreply.github.com>
2019-06-09 22:45:55 +0200

    Datos del tutor

---
+++ app/src/main/java/com/example/apperasmus/ActivityNuevoMensaje.java
@@ -0,0 +1,13 @@
+package com.example.apperasmus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityNuevoMensaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_mensaje);
+    }
+}
