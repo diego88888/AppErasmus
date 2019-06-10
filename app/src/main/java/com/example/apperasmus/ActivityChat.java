d2cf5f4b2ee7eb6b8e3fec8b95f28c878357152d
diego88888 <45438301+diego88888@users.noreply.github.com>
2019-06-10 02:28:13 +0200

    Pruebas chat

--- app/src/main/java/com/example/apperasmus/ActivityChat.java
+++ app/src/main/java/com/example/apperasmus/ActivityChat.java
@@ -1,9 +1,13 @@
 package com.example.apperasmus;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ActivityChat extends AppCompatActivity {
@@ -28,7 +32,11 @@ public class ActivityChat extends AppCompatActivity {
         botonNuevoMensaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(getApplicationContext(), ActivityNuevoMensaje.class);
+                i.putExtra("USUARIOALUMNO", uA);
+                i.putExtra("USUARIOTUTOR", uT);
+                getApplicationContext().startActivity(i);
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/apperasmus/ActivityNuevoMensaje.java
+++ app/src/main/java/com/example/apperasmus/ActivityNuevoMensaje.java
@@ -1,13 +1,40 @@
 package com.example.apperasmus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ActivityNuevoMensaje extends AppCompatActivity {
-
+public class ActivityNuevoMensaje extends AppCompatActivity{
+    Button btnEnviarMensaje;
+    EditText etMensaje;
+    UsuarioTutor uT;
+    UsuarioAlumno uA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_mensaje);
+
+        btnEnviarMensaje = (Button) findViewById(R.id.btnEnviarMensaje);
+        etMensaje = (EditText) findViewById(R.id.etMensaje);
+
+        Bundle b = getIntent().getExtras();
+        if(b!=null){
+            uT =  b.getParcelable("USUARIOTUTOR");
+            uA =  b.getParcelable("USUARIOALUMNO");
+        }
+
+        btnEnviarMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ActivityChat.class);
+                i.putExtra("USUARIOALUMNO", uA);
+                i.putExtra("USUARIOTUTOR", uT);
+                getApplicationContext().startActivity(i);
+                finish();
+            }
+        });
     }
 }
